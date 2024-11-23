ff5300844f9e46b0ed063216e59b8e8ae16040f1
Al Cutter <al-github@9600.org>
2024-11-23 12:36:01 +0000

    Start implementing a sync method to store data locally

--- cmd/octonaut/main.go
+++ cmd/octonaut/main.go
@@ -5,10 +5,8 @@ import (
 	"database/sql"
 	"flag"
 	"strings"
-	"time"
 
 	"github.com/AlCutter/octonaut/internal/octonaut"
-	"github.com/AlCutter/octonaut/internal/octopus"
 	_ "github.com/mattn/go-sqlite3"
 	"k8s.io/klog/v2"
 )
@@ -23,46 +21,19 @@ var (
 
 func main() {
 	flag.Parse()
-	o := mustNewFromFlags()
 
 	ctx := context.Background()
-
-	a, err := o.Account(ctx)
-	if err != nil {
-		klog.Exitf("Account: %v", err)
-	}
-	klog.Infof("Account: %+v", a)
-
-	var elecMeter *octopus.ElectricityMeterPoints
-	var agreement *octopus.Agreement
-	now := time.Now()
-
-search:
-	for _, p := range a.Properties {
-		for _, e := range p.ElectricityMeterPoints {
-			if len(e.ActiveMeters()) > 0 {
-				agreement = e.ActiveAgreement(now)
-				elecMeter = &e
-				elecMeter.Meters = elecMeter.ActiveMeters()
-				break search
-			}
+	c, o := mustNewFromFlags(ctx)
+	defer func() {
+		if err := c(); err != nil {
+			klog.Warningf("close: %v", err)
 		}
-	}
-	if elecMeter == nil {
-		klog.Exitf("Couldn't find electricity meterpoint with active meters")
+	}()
+
+	if err := o.Sync(ctx); err != nil {
+		klog.Exitf("Sync: %v", err)
 	}
 
-	klog.Infof("Agreement: %+v", agreement)
-
-	products, err := o.Products(ctx, &agreement.ValidFrom)
-	if err != nil {
-		klog.Exitf("Products: %v", err)
-	}
-	tariffProduct := products.FindByTariff(agreement.TariffCode)
-	if tariffProduct == nil {
-		klog.Exitf("Couldn't find product for code %q", agreement.TariffCode)
-	}
-	klog.Infof("Tariff Product: %+v", tariffProduct)
 	/*
 		cons, err := c.Consumption(ctx, elecMeter.MPAN, elecMeter.Meters[0].SerialNumber, now.Add(-30*24*time.Hour), now)
 		if err != nil {
@@ -71,26 +42,30 @@ search:
 		klog.Infof("Cons: %+v", cons)
 	*/
 
-	_, err = o.TariffRates(ctx, tariffProduct.Code, "electricity", agreement.TariffCode, "standard-unit-rates", now.Add(-30*24*time.Hour), now)
-	if err != nil {
-		klog.Exitf("Tariff: %v", err)
-	}
+	/*
+		_, err = o.TariffRates(ctx, tariffProduct.Code, "electricity", agreement.TariffCode, "standard-unit-rates", now.Add(-30*24*time.Hour), now)
+		if err != nil {
+			klog.Exitf("Tariff: %v", err)
+		}
+	*/
 	//	klog.Infof("Tariff: %+v", tariff)
 }
 
-func mustNewFromFlags() *octonaut.Octonaut {
+func mustNewFromFlags(ctx context.Context) (func() error, *octonaut.Octonaut) {
 	db, err := sql.Open("sqlite3", *dbPath)
 	if err != nil {
 		klog.Exitf("Failed to open DB (%q): %v", *dbPath, err)
 	}
-	defer db.Close()
 
 	u := *ep
 	if !strings.HasSuffix(u, "/") {
 		u += "/"
 	}
 
-	r := octonaut.New(*account, *key, u, db)
+	r, err := octonaut.New(ctx, *account, *key, u, db)
+	if err != nil {
+		klog.Exitf("New: %v", err)
+	}
 
-	return r
+	return db.Close, r
 }
--- internal/octonaut/octonaut.go
+++ internal/octonaut/octonaut.go
@@ -3,6 +3,8 @@ package octonaut
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 	"strings"
 	"time"
 
@@ -14,7 +16,7 @@ type Octonaut struct {
 	db *sql.DB
 }
 
-func New(a, k, ep string, db *sql.DB) *Octonaut {
+func New(ctx context.Context, a, k, ep string, db *sql.DB) (*Octonaut, error) {
 	if !strings.HasSuffix(ep, "/") {
 		ep += "/"
 	}
@@ -28,7 +30,128 @@ func New(a, k, ep string, db *sql.DB) *Octonaut {
 		db: db,
 	}
 
-	return r
+	return r, initDB(ctx, r.db)
+}
+
+func initDB(ctx context.Context, db *sql.DB) error {
+	if _, err := db.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS Account(
+			Number string NOT NULL PRIMARY KEY
+		);
+		`); err != nil {
+		return fmt.Errorf("failed to create Account table: %v", err)
+	}
+	if _, err := db.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS Property(
+			ID string NOT NULL,
+			Account string NOT NULL,
+			MovedInAt DATETIME NOT NULL,
+			MovedOutAt DATETIME,
+			PRIMARY KEY (ID),
+			FOREIGN KEY (Account) REFERENCES Account(Number)
+		);
+		`); err != nil {
+		return fmt.Errorf("failed to create Property table: %v", err)
+	}
+	if _, err := db.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS Meter(
+			Property string NOT NULL,
+			MPAN string NOT NULL,
+			Serial string NOT NULL,
+			PRIMARY KEY (Property, MPAN, Serial),
+			FOREIGN KEY (Property) REFERENCES Property(ID)
+		);
+		`); err != nil {
+		return fmt.Errorf("failed to create Meter table: %v", err)
+	}
+	if _, err := db.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS MeterRate(
+			Meter string NOT NULL,
+			Rate string NOT NULL,
+			PRIMARY KEY (Meter, Rate),
+			FOREIGN KEY (Meter) REFERENCES Meter(ID)
+		);
+		`); err != nil {
+		return fmt.Errorf("failed to create MeterRate table: %v", err)
+	}
+
+	if _, err := db.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS Consumption(
+			Account string,
+			MPAN	string,
+			Meter	string,
+			At		DateTime,
+			Seconds	LONG INT,
+			kWh		REAL,
+			PRIMARY KEY (Account, MPAN, Meter, At));
+		`); err != nil {
+		return fmt.Errorf("create Consumption table failed: %v", err)
+	}
+	if _, err := db.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS TariffRate(
+			Code	string,
+			At		DateTime,
+			PerUnit REAL,
+			PRIMARY KEY (Code, At));
+		`); err != nil {
+		return fmt.Errorf("create TariffRate table failed: %v", err)
+	}
+	return nil
+}
+
+func (o Octonaut) Sync(ctx context.Context) error {
+	a, err := o.Account(ctx)
+	if err != nil {
+		return err
+	}
+	if err := o.upsertAccount(ctx, a); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *Octonaut) upsertAccount(ctx context.Context, a octopus.Account) error {
+	tx, err := o.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if tx != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if _, err := tx.ExecContext(ctx, `INSERT OR REPLACE INTO Account VALUES(?)`, a.Number); err != nil {
+		return fmt.Errorf("insert/update account: %v", err)
+	}
+
+	for _, p := range a.Properties {
+		if _, err := tx.ExecContext(ctx, `INSERT OR REPLACE INTO Property VALUES(?, ?, ?, ?)`, p.ID, a.Number, p.MovedInAt, p.MovedOutAt); err != nil {
+			return fmt.Errorf("insert/update property: %v", err)
+		}
+		for _, em := range p.ElectricityMeterPoints {
+			for _, m := range em.Meters {
+				if _, err := tx.ExecContext(ctx, `INSERT OR REPLACE INTO Meter VALUES(?, ?, ?)`, p.ID, em.MPAN, m.SerialNumber); err != nil {
+					return fmt.Errorf("insert/update Meter: %v", err)
+				}
+			}
+		}
+	}
+
+	return tx.Commit()
+}
+
+func (o *Octonaut) nextTariffRate(ctx context.Context, code string) (*time.Time, error) {
+	r := o.db.QueryRowContext(ctx, "SELECT MAX(At) FROM TariffRate WHERE Code == ?", code)
+	var at time.Time
+	if err := r.Scan(&at); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return &time.Time{}, nil
+		}
+		return nil, fmt.Errorf("failed to scan latest tariffrate.at: %v", err)
+	}
+	return &at, nil
 }
 
 func (o *Octonaut) Account(ctx context.Context) (octopus.Account, error) {
